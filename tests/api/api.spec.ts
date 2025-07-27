780fc44712657305d37ac57a59aada7348bf4847
bhimpd <dreamypd73@gmail.com>
2025-07-27 21:14:16 +0545

    made resubale function for the successful message assertion

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -2,6 +2,8 @@ import {test,expect} from "@playwright/test";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { assertSuccessfulResponse,assertUserDataResponse } from "./utilis/validator";
+
 const baseURL = process.env.API_BASEURL;
 const apiKey = process.env.API_KEY;
 
@@ -10,7 +12,7 @@ if (!baseURL || !apiKey) {
 }
 
 
-test("GET: API :Fetch the Users Data", async ({ request }) => {
+test("GET: API :Fetch the Users Data- default Pagination", async ({ request }) => {
   console.log("BASE URL ::  ", baseURL);
 
   const response = await request.get(`${baseURL}/users`, {
@@ -19,80 +21,19 @@ test("GET: API :Fetch the Users Data", async ({ request }) => {
     }
   });
 
-  expect(response.status()).toBe(200);
-  const data = await response.json();
-
-  console.log("DATA ::: ", data);
-
-  expect (data).toHaveProperty("page");
-  expect (Number.isInteger(data.page)).toBe(true);
-
-  expect (data).toHaveProperty("per_page");
-  expect (Number.isInteger(data.per_page)).toBe(true);
-
-  expect (data).toHaveProperty("total");
-  expect (Number.isInteger(data.total)).toBe(true);
-
-  expect (data).toHaveProperty("total_pages");
-  expect (Number.isInteger(data.total_pages)).toBe(true);
-
-  expect (data).toHaveProperty("data");
-  expect (Array.isArray(data.data)).toBe(true);
-
+  const data = await assertSuccessfulResponse(response); 
+ 
   const dataLength = data.data.length;
+
   console.log("User data length :: ",dataLength);
 
   expect (dataLength).toBeGreaterThan(0);
-  expect (dataLength).toBeLessThanOrEqual(15);
+  expect (dataLength).toBeLessThanOrEqual(6);
 
   const usersData = data.data;
 
   usersData.forEach((user: any, index:number) =>{
-
-    try {
-
-      expect(user).toHaveProperty("id");
-      expect (Number.isInteger(user.id)).toBe(true);
-      expect(user.id).toBeGreaterThan(0);
-  
-      expect(user).toHaveProperty("first_name");
-      expect(typeof user.first_name).toBe("string");
-      expect(user.first_name.trim().length).toBeGreaterThan(0);
-  
-      expect(user).toHaveProperty("last_name");
-      expect(typeof user.last_name).toBe("string");
-      expect(user.last_name.trim().length).toBeGreaterThan(0);
-
-
-      // --- Email ---
-      expect(user).toHaveProperty("email");
-      expect(typeof user.email).toBe("string");
-      expect(user.email.trim().length).toBeGreaterThan(0);
-
-      // Validate email format using regex
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      expect(emailRegex.test(user.email)).toBe(true);
-
-      // --- Avatar ---
-      expect(user).toHaveProperty("avatar");
-      expect(typeof user.avatar).toBe("string");
-      expect(user.avatar.trim().length).toBeGreaterThan(0);
-
-      // Basic check if avatar is a valid URL
-      const urlRegex = /^https?:\/\/[^\s$.?#].[^\s]*$/;
-      expect(urlRegex.test(user.avatar)).toBe(true);
-
-      // Optional: Check it ends with .jpg or .png (basic image check)
-      expect(user.avatar.endsWith(".jpg") || user.avatar.endsWith(".png") || user.avatar.endsWith(".jpeg")).toBe(true);
-      
-      
-    } catch (error) {
-       throw new Error(
-            `Validation failed at index ${index}:\n${JSON.stringify(user, null, 2)}\nError: ${error}`
-          );
-    }
-  
-
+    assertUserDataResponse(user,index);
   });
 
 });
---
+++ tests/api/utilis/validator.ts
@@ -0,0 +1,78 @@
+import {expect } from "@playwright/test";
+
+export async function assertSuccessfulResponse(response: any){
+    
+    expect(response.status()).toBe(200);
+    const data = await response.json();
+  
+    console.log("DATA ::: ", data);
+  
+    expect (data).toHaveProperty("page");
+    expect (Number.isInteger(data.page)).toBe(true);
+    expect ((data.page)).toBe(1);
+  
+  
+    expect (data).toHaveProperty("per_page");
+    expect (Number.isInteger(data.per_page)).toBe(true);
+    expect ((data.per_page)).toBe(6);
+  
+  
+    expect (data).toHaveProperty("total");
+    expect (Number.isInteger(data.total)).toBe(true);
+  
+    expect (data).toHaveProperty("total_pages");
+    expect (Number.isInteger(data.total_pages)).toBe(true);
+  
+    expect (data).toHaveProperty("data");
+    expect (Array.isArray(data.data)).toBe(true);
+    
+    return data;
+  
+}
+
+export async function assertUserDataResponse(user: any, index: number){
+  
+    try {
+
+        expect(user).toHaveProperty("id");
+        expect (Number.isInteger(user.id)).toBe(true);
+        expect(user.id).toBeGreaterThan(0);
+    
+        expect(user).toHaveProperty("first_name");
+        expect(typeof user.first_name).toBe("string");
+        expect(user.first_name.trim().length).toBeGreaterThan(0);
+    
+        expect(user).toHaveProperty("last_name");
+        expect(typeof user.last_name).toBe("string");
+        expect(user.last_name.trim().length).toBeGreaterThan(0);
+  
+  
+        // --- Email ---
+        expect(user).toHaveProperty("email");
+        expect(typeof user.email).toBe("string");
+        expect(user.email.trim().length).toBeGreaterThan(0);
+  
+        // Validate email format using regex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        expect(emailRegex.test(user.email)).toBe(true);
+  
+        // --- Avatar ---
+        expect(user).toHaveProperty("avatar");
+        expect(typeof user.avatar).toBe("string");
+        expect(user.avatar.trim().length).toBeGreaterThan(0);
+  
+        // Basic check if avatar is a valid URL
+        const urlRegex = /^https?:\/\/[^\s$.?#].[^\s]*$/;
+        expect(urlRegex.test(user.avatar)).toBe(true);
+  
+        // Optional: Check it ends with .jpg or .png (basic image check)
+        expect(user.avatar.endsWith(".jpg") || user.avatar.endsWith(".png") || user.avatar.endsWith(".jpeg")).toBe(true);
+        
+        
+        
+      } catch (error) {
+         throw new Error(
+              `Validation failed at index ${index}:\n${JSON.stringify(user, null, 2)}\nError: ${error}`
+            );
+      }
+}
