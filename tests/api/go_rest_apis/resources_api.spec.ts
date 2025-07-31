4659a78d28c7d0f1621f5db3e79358426694a98c
bhimpd <dreamypd73@gmail.com>
2025-07-31 08:49:19 +0545

    added test case for gorest api

---
+++ tests/api/go_rest_apis/resources_api.spec.ts
@@ -0,0 +1,28 @@
+import {test, expect, request} from '@playwright/test';
+import dotenv from "dotenv";
+dotenv.config();
+
+const goRestBaseURL =  process.env.GOREST_BASEURL;
+const token = process.env.GOREST_TOKEN;
+
+if (!goRestBaseURL || !token){
+    throw new Error("GOREST_BASEURL or TOKEN is not defined in your .env file..Check it Out.....");
+}
+
+test.describe("Users API...", () => {
+    test("POSITIVE : GET API - Fetch the users", async({request}) => {
+
+        const response = await request.get(`${goRestBaseURL}/users`, {
+
+        });
+
+        console.log("BASEURLLL ::",goRestBaseURL);
+
+        expect (response.status()).toBe(200);
+        const body = await response.json();
+
+        console.log("BODY DATA :: ",  body)
+
+
+    });
+})
