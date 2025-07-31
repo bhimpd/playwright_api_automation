c214aea94ea627bda066e9481ae6492e67e94f18
bhimpd <dreamypd73@gmail.com>
2025-07-31 09:47:14 +0545

    added full assertionfor the user data

--- tests/api/go_rest_apis/helper/helperFunction.ts
+++ tests/api/go_rest_apis/helper/helperFunction.ts
@@ -10,3 +10,24 @@ export async function assertNonEmptyString(value:any){
     expect (typeof value).toBe("string");
     expect (value.trim().length).toBeGreaterThan(0);
 }
+
+export async function assertNonEmptyArray(value:any){
+    expect(Array.isArray(value)).toBe(true);
+    expect (value.length).toBeGreaterThan(0);
+}
+
+export async function assertValidEmail(email:any){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    expect (emailRegex.test(email)).toBe(true);
+}
+
+
+export async function assertGender(value: any) {
+    const expectedGender = ["male", "female"];
+    expect(expectedGender.includes(value)).toBe(true);
+}
+
+export async function assertStatus(value: any) {
+    const expectedStatus = ["active", "inactive"];
+    expect(expectedStatus.includes(value)).toBe(true);
+}
--- tests/api/go_rest_apis/resources_api.spec.ts
+++ tests/api/go_rest_apis/resources_api.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import dotenv from "dotenv";
 dotenv.config();
 
-import { assertPositiveInteger,assertNonEmptyString } from './helper/helperFunction';
+import { assertPositiveInteger,assertNonEmptyString, assertNonEmptyArray, assertValidEmail, assertGender, assertStatus } from './helper/helperFunction';
 
 const goRestBaseURL = process.env.GOREST_BASEURL;
 const token = process.env.GOREST_TOKEN;
@@ -54,22 +54,28 @@ test.describe("Users API...", () => {
         expect(Array.isArray(body)).toBe(true);
         expect(body.length).toBeGreaterThan(0);
 
+        const idSet = new Set();
+
         // Loop over each object and check required properties
         for (const user of body) {
             expect(user).toHaveProperty('id');
             assertPositiveInteger(user.id);
 
             expect(user).toHaveProperty('name');
-            assertNonEmptyString(user.id);
+            assertNonEmptyString(user.name);
 
             expect(user).toHaveProperty('email');
             assertNonEmptyString(user.email);
+            assertValidEmail(user.email)
 
             expect(user).toHaveProperty('gender');
             assertNonEmptyString(user.gender);
+            assertGender(user.gender);
 
             expect(user).toHaveProperty('status');
             assertNonEmptyString(user.status);
+            assertStatus(user.status);
+
         }
 
     });
